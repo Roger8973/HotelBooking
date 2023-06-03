18403ca39a47a70ff2554f134259707eadee0124
Roger8973 <88730871+Roger8973@users.noreply.github.com>
2023-06-03 21:03:28 +0000

    Correção de arquivos

--- Guest.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-public class Guest
-{
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public string SurName { get; set; }
-    public string Email { get; set; }
-}
