8152ee2320fd5259d6a01735b61c541980064cce
roger.fraga <roger.fraga@outlook.com>
2023-06-03 18:27:39 -0300

    Metodo post da api finalizado

---
+++ BookingService/Consumers/API/Controllers/GuestController.cs
@@ -0,0 +1,43 @@
+﻿using Application.Guest.DTO;
+using Application.Guest.Ports;
+using Application.Guest.Requests;
+using Application.Guest.Responses;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class GuestController : Controller
+    {
+        private readonly ILogger<GuestController> _logger;
+        private readonly IGuestManager _guestManager;
+
+        public GuestController(ILogger<GuestController> logger, IGuestManager guestManager)
+        {
+            _logger = logger;
+            _guestManager = guestManager;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<GuestDTO>> Post(GuestDTO guest)
+        {
+            var request = new CreateGuestRequest
+            {
+                Data = guest,
+            };
+
+            var response = await _guestManager.CreateGuest(request);
+
+            if (response.Success) 
+                return Created("", request.Data);
+
+            if (response.ErrorCode == Application.ErrosCodes.NOT_FOUND)
+                return BadRequest(response);
+
+            _logger.LogError("Response with unknown ErroCode Returned", response);
+
+            return BadRequest(500);
+        }
+    }
+}
--- BookingService/Consumers/API/Program.cs
+++ BookingService/Consumers/API/Program.cs
@@ -1,4 +1,8 @@
+using Application;
+using Application.Guest.Ports;
 using Data;
+using Data.Guest;
+using Domain.Ports;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,6 +10,11 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+#region Ioc
+builder.Services.AddScoped<IGuestManager, GuestManager>();
+builder.Services.AddScoped<IGuestRepository, GuestRepository>();
+#endregion
 
 var connnectionString = builder.Configuration.GetConnectionString("Main");
 
