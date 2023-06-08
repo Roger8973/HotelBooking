b81cd5b7f5da62fd5d0a1aa4bc1e36880adb2185
roger.fraga <roger.fraga@outlook.com>
2023-06-08 11:17:44 -0300

    Implementada validação de email com Regex

--- BookingService/Core/Domain/Utils.cs
+++ BookingService/Core/Domain/Utils.cs
@@ -1,17 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Globalization;
+using System.Text.RegularExpressions;
 
 namespace Domain
 {
     public class Utils
     {
-        //Email validations will be integrated
         public static bool ValidadeEmail(string email)
         {
-            if (email == "bb@.com") return false; return true;
-        } 
+            if (string.IsNullOrWhiteSpace(email))
+                return false;
+
+            try
+            {
+                // Normalize the domain
+                email = Regex.Replace(email, @"(@)(.+)$", DomainMapper,
+                                      RegexOptions.None, TimeSpan.FromMilliseconds(200));
+
+                if (string.IsNullOrEmpty(email))
+                    return false;
+
+                // Return true if strIn is in valid email format.
+                return Regex.IsMatch(email,
+                      @"^(?("")(""[^""]+?""@)|(([0-9a-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))" +
+                      @"(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-z][-\w]*[0-9a-z]*\.)+[a-z0-9][-\w]*[a-z0-9]))$");
+            }
+            catch (RegexMatchTimeoutException)
+            {
+                return false;
+            }
+        }
+        private static string DomainMapper(Match match)
+        {
+            // Use IdnMapping class to convert Unicode domain names.
+            IdnMapping idn = new IdnMapping();
+
+            // Pull out and process domain name (throws ArgumentException on invalid)
+            string domainName = idn.GetAscii(match.Groups[2].Value);
+
+            return match.Groups[1].Value + domainName;
+        }
     }
 }
